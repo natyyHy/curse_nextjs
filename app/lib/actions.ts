6378b1ca89a74564c9f0582078e352c2b41e4c83
natyyHy <natielegrazielly5@gmail.com>
2025-12-07 14:34:37 -0300

    commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,6 +113,7 @@ export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
+        redirect('/dashboard/invoices');
     } catch (error) {
         return {
             message: 'Database Error: Failed to Delete Invoice.',
